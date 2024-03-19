77e81623f530cdd448c6ee92d26b469ba99d6b76
DannyM15 <merinodanny15@gmail.com>
2024-03-18 22:55:45 -0700

    need to finish update employee

--- index.js
+++ index.js
@@ -15,31 +15,47 @@ const db = mysql.createConnection(
   console.log(`Connected to the courses_db database.`)
 );
 
-inquirer.prompt([
+
+const prompt = () => {
+
+  inquirer.prompt([
     {
-        type: 'list',
-        name: 'start',
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role'
-        ]
+      type: 'list',
+      name: 'start',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
     },
     {
       type: 'input',
       name: 'AddDept',
       message: 'What is the name of the department?',
-      when: (answer) => answers.start === 'Add a department'
-    },    
+      when: (answer) => answer.start === 'Add a department'
+    },
     {
       type: 'input',
-      name: 'AddRole',
+      name: 'AddRole1',
       message: 'What is the name of the role?',
+      when: (answer) => answer.start === 'Add a role',
+    },
+    {
+      type: 'input',
+      name: 'AddRole2',
+      message: 'Which department does the role belong to?',
+      when: (answer) => answer.start === 'Add a role',
+    },
+    {
+      type: 'input',
+      name: 'AddRole3',
+      message: 'What is the Salary for the role?',
       when: (answer) => answer.start === 'Add a role',
     },
     {
@@ -59,64 +75,99 @@ inquirer.prompt([
       name: 'AddEmployee3',
       message: 'What is the role of the employee?',
       when: (answer) => answer.start === 'Add an employee',
+    },
+    {
+      type: 'list',
+      name: 'updateEmp',
+      message: 'Who would you like to update?',
+      choices: db.employees,
+      when: (answer) => answer.start === 'Update an employee role',
     }
-])
-.then((data) => {
-  if(data.start === 'View all departments'){
-    db.query('SELET * FROM deparments', (error, results) =>{
-      if (error) {
-        console.error(error);
-      }
-      else {
-        console.log(results);
-      }
-    })
-  }
-  if(data.start === 'View all roles'){
-    db.query('SELECT * FROM roles', (error, results) =>{
-      if (error) {
-        console.error(error);
-      }
-      else {
-        console.log(results);
-      }
-    })
-  }
-  if(data.start === 'View all employees'){
-    db.query('SELECT * FROM employees', (error, results) =>{
-      if (error) {
-        console.error(error);
-      }
-      else {
-        console.log(results);
-      }
-    })
-  }
-  if(data.start === 'add a department'){
-    db.query('INSERT INTO departments VALUES ', results, (error, results) =>{
-      if (error) {
-        console.error(error);
-      }
-      else {
-        console.log(results + ' added to deparments');
-      }
-    })
-  }
-  if(data.start === 'add a role'){
-    db.query('INSERT INTO roles VALUES ', results, (error, results) =>{
-      if (error) {
-        console.error(error);
-      }
-      else {
-        console.log(results + ' added to roles');
-      }
-    })
-  }
-  if(data.start === 'add an employee'){
+  ])
 
-  }
-  if(data.start === 'update an employee role'){
+    .then((data) => {
+      if (data.start === 'Add a department') {
+        db.query('INSERT INTO departments (department_name) VALUES (?) ', [data.AddDept], (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.table(results);
+            prompt();
+          }
+        })
+      }
+      if (data.start === 'View all departments') {
+        db.query('SELECT * FROM departments', (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.table(results);
+            prompt();
+          }
+        })
+      }
+      if (data.start === 'View all roles') {
+        db.query('SELECT * FROM roles', (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.table(results);
+            prompt();
+          }
+        })
+      }
+      if (data.start === 'View all employees') {
+        db.query('SELECT * FROM employees', (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.table(results);
+            prompt();
 
-  }
-})
+          }
+        })
+      }
+      if (data.start === 'Add a role') {
+        db.query('INSERT INTO roles (role_name, department_id, salary) VALUES (?) ', [data.AddRole1, data.AddRole2, data.AddRole3], (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.table(results);
+            prompt();
+          }
+        })
+      }
+      if (data.start === 'Add an employee') {
+        db.query('INSERT INTO employees (first_name, last_name, role_id) VALUES (?) ', [data.AddEmployee1, data.AddEmployee2, data.AddEmployee3], (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.table(results);
+            prompt();
+          }
+        })
+      }
+      if (data.start === 'Update an employee role') {
+        db.query('UPDATE  ', results, (error, results) => {
+          if (error) {
+            console.error(error);
+          }
+          else {
+            console.log(results + ' added to roles');
 
+          }
+        })
+      }
+      if (data.start === 'Exit') {
+        console.log('"Heres looking at you, kid"')
+      }
+    })
+}
+
+prompt();
